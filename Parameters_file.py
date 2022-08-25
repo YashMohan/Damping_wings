691d83969fcc1d6a9f3c84ccdfec50f1195b0882
Yash Mohan Sharma <yashmohansharma96@gmail.com>
2022-08-25 15:24:30 +0200

    Update Parameters_file.py

--- Parameters_file.py
+++ Parameters_file.py
@@ -3,7 +3,9 @@ Parameters = {
     'M_min':9,
     'T_vir':5,
     'target_xh':0.75,
-    'alpha_esc':1.0,
+    'alpha_esc':0.2,
+    'alpha_star': 0,
+    'f_star':-0.25,
     'DIM':1024,
     'HII_DIM':256,
     'BOX_LEN':200
